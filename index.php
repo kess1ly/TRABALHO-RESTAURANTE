b6b706d5689ac4e679068d9b50765f4a2899bbe1
kess1ly <kekezinha2905@gmail.com>
2026-08-19 16:49:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,57 +2,101 @@
 
 require_once "conexao.php";
 
-$sql ="SELECT * FROM clientes ORDER BY id DESC";
+$sql = "SELECT * FROM clientes ORDER BY id DESC";
 
-$resultado =$conexao->query($sql);
+$resultado = $conexao->query($sql);
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clientes - Restarante</title>
+
+    <title>Clientes - Restaurante</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
+
+<header>
+    <h2> Restaurante</h2>
+</header>
+
+<div class="container">
+
     <h1>Clientes</h1>
-    <a href="cadastro.php">Cadastrar novo cliente</a>
+
+    <a class="botao" href="cadastro.php">
+        + Cadastrar novo cliente
+    </a>
 
     <br><br>
 
-    <table border="1">
+    <table>
 
-    <tr>
-        <th>ID</th>
-        <th>Nome</th>
-        <th>CPF</th>
-        <th>Telefone</th>
-        <th>E-mail</th>
-        <th>Endereço</th>
-        <th>Data de cadastro</th>
-        <th>Ações</th>
-</tr>
-<?php while ($cliente = $resultado->fetch_assoc()){?>
-    <tr>
-       <td><?= htmlspecialchars($cliente['id']) ?></td>
-                <td><?= htmlspecialchars($cliente['nome']) ?></td>
-                <td><?= htmlspecialchars($cliente['cpf']) ?></td>
-                <td><?= htmlspecialchars($cliente['telefone']) ?></td>
-                <td><?= htmlspecialchars($cliente['email']) ?></td>
-                <td><?= htmlspecialchars($cliente['endereco']) ?></td>
-                <td><?= htmlspecialchars($cliente['data_cadastro']) ?></td>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>CPF</th>
+            <th>Telefone</th>
+            <th>E-mail</th>
+            <th>Data de cadastro</th>
+            <th>Ações</th>
+        </tr>
+
+        <?php while ($cliente = $resultado->fetch_assoc()) { ?>
+
+            <tr>
 
                 <td>
-                    <a href="editar.php?id=<?= $cliente['id'] ?>">Editar</a>
-                    |
-                    <a href="excluir.php?id=<?= $cliente['id'] ?>">Excluir</a>
+                    <?= htmlspecialchars($cliente['id']) ?>
                 </td>
+
+                <td>
+                    <?= htmlspecialchars($cliente['nome']) ?>
+                </td>
+
+                <td>
+                    <?= htmlspecialchars($cliente['cpf']) ?>
+                </td>
+
+                <td>
+                    <?= htmlspecialchars($cliente['telefone']) ?>
+                </td>
+
+                <td>
+                    <?= htmlspecialchars($cliente['email']) ?>
+                </td>
+
+                <td>
+                    <?= htmlspecialchars($cliente['data_cadastro']) ?>
+                </td>
+
+                <td class="acoes">
+
+                    <a href="editar.php?id=<?= $cliente['id'] ?>">
+                        Editar
+                    </a>
+
+                    |
+
+                    <a href="excluir.php?id=<?= $cliente['id'] ?>">
+                        Excluir
+                    </a>
+
+                </td>
+
             </tr>
 
         <?php } ?>
 
     </table>
+
+</div>
 
 </body>
 </html>
