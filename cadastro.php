5d8bac5b94c7fe8c56801d2daaf21e50332af4b1
kess1ly <kekezinha2905@gmail.com>
2026-08-19 16:51:24 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,42 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cadastrar Cliente</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <h1>Cadastrar Cliente</h1>
 
-    <form action="salvar.php" method="POST">
+<header>
+    <h2>Restaurante</h2>
+</header>
 
-    <label>Nome:</label>
-    <input type="text" name="nome" required>
-    <br><br>
+<div class="container">
 
-    <label>CPF:</label>
-    <input type="text" name="cpf" required>
-    <br><br>
+    <div class="formulario">
 
-    <label>Telefone:</label>
-    <input type="tel" name="telefone"required>
-    <br><br>
+        <h1>Cadastrar Cliente</h1>
 
-    <label>E-mail:</label>
-    <input type="email" name="email" required>
-    <br><br>
+      
 
-    <label>Endereço</label>
-    <input type="text" name="endereco" required>
-    <br><br>
+        <form action="salvar.php" method="POST">
 
-    <button type="submit">Cadastrar</button>
+            <label>Nome:</label>
+            <input type="text" name="nome" required>
 
-</form>
+            <label>CPF:</label>
+            <input type="text" name="cpf" required>
 
-<br>
+            <label>Senha:</label>
+            <input type="password" name="senha" required>
 
-<a href="index.php">Voltar para clientes</a>
+            <label>Telefone:</label>
+            <input type="text" name="telefone" required>
+
+            <label>E-mail:</label>
+            <input type="email" name="email" required>
+
+           
+            <button type="submit">Cadastrar</button>
+
+        </form>
+
+        <br>
+
+        <a href="index.php">Voltar para clientes</a>
+
+    </div>
+
+</div>
 
 </body>
 </html>
