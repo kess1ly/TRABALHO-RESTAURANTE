a37e45f40d0f4fa643372acab57d4f9996cea82e
kess1ly <kekezinha2905@gmail.com>
2026-08-19 16:45:58 -0300

    Add files via upload

--- restaurante.sql
+++ restaurante.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19/08/2026 às 04:54
+-- Tempo de geração: 19/08/2026 às 21:44
 -- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Versão do PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,16 +33,16 @@ CREATE TABLE `clientes` (
   `cpf` varchar(11) NOT NULL,
   `telefone` varchar(20) NOT NULL,
   `email` varchar(150) NOT NULL,
-  `endereco` varchar(150) NOT NULL,
-  `data_cadastro` date NOT NULL
+  `data_cadastro` date NOT NULL,
+  `senha` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `clientes`
 --
 
-INSERT INTO `clientes` (`id`, `nome`, `cpf`, `telefone`, `email`, `endereco`, `data_cadastro`) VALUES
-(4, 'Alyne Dias', '2343567890', '43567898', 'alynedias49@gmail.com', 'montes claros', '2026-08-18');
+INSERT INTO `clientes` (`id`, `nome`, `cpf`, `telefone`, `email`, `data_cadastro`, `senha`) VALUES
+(10, 'davi fernandes', '1123423424', '38998989394', 'gabizinh@gmail.com', '2026-08-19', '$2y$10$IqSNDlypQGJ1T3P4IYkYQOlXeCGGvkcU0sWpG/08zo8hp4p8reb42');
 
 -- --------------------------------------------------------
 
@@ -79,13 +79,20 @@ CREATE TABLE `pedidos` (
 --
 
 CREATE TABLE `pratos` (
-  `id_prato` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `descricao` text DEFAULT NULL,
   `categoria` varchar(50) NOT NULL,
   `preco` decimal(10,2) NOT NULL,
   `disponibilidade` char(1) NOT NULL DEFAULT 'S'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `pratos`
+--
+
+INSERT INTO `pratos` (`id`, `nome`, `descricao`, `categoria`, `preco`, `disponibilidade`) VALUES
+(4, 'petit gateu', 'doce doçudo', 'sobremesa', 35.00, 'D');
 
 --
 -- Índices para tabelas despejadas
@@ -116,7 +123,7 @@ ALTER TABLE `pedidos`
 -- Índices de tabela `pratos`
 --
 ALTER TABLE `pratos`
-  ADD PRIMARY KEY (`id_prato`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -126,7 +133,7 @@ ALTER TABLE `pratos`
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de tabela `itens_pedido`
@@ -144,7 +151,7 @@ ALTER TABLE `pedidos`
 -- AUTO_INCREMENT de tabela `pratos`
 --
 ALTER TABLE `pratos`
-  MODIFY `id_prato` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restrições para tabelas despejadas
